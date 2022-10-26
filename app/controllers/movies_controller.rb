bd5c2773cad3c591535b8dd0cceff99ba429c7e7
oshiro <kaito.o.0628@gmail.com>
2022-10-26 09:22:27 +0900

    動画教材ページにページネーションの実装

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,8 @@
 class MoviesController < ApplicationController
+  # 1ページの表示数
+  PER_PAGE = 12
+
   def index
-    @movies = Movie.where(genre: Movie::RAILS_GENRE_LIST)
+    @movies = Movie.page(params[:page]).per(PER_PAGE)
   end
 end
