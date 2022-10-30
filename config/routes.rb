454ff90f947d38e5e35fce76c921b035e668256b
FMY <n.fmy.0526@icloud.com>
2022-10-30 19:43:45 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   devise_for :users
   root "texts#index"
   resources :texts, only: [:index, :show]
-  resources :movies, only: [:index]
+  resources :movies, only: [:index] do
+    resource :watch_progresses, only: [:create, :destroy]
+  end
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
