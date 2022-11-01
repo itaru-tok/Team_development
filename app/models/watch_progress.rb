4a62b000f800c51de23b4e1db1a7205a8a3026e8
FMY <n.fmy.0526@icloud.com>
2022-11-01 12:30:55 +0900

    日本語訳を追加

--- app/models/watch_progress.rb
+++ app/models/watch_progress.rb
@@ -3,6 +3,6 @@ class WatchProgress < ApplicationRecord
   belongs_to :movie
   validates :user_id, uniqueness: {
     scope: :movie_id,
-    message: "は同じ動画教材を2回以上視聴済みにはできません"
+    message: :duplicated
   }
 end
