ecb64b431e603dbb595c4e71db97746b6db002ed
itaruby_on_rails <98925848+itaru-tokuda@users.noreply.github.com>
2022-11-13 13:22:21 +0800

    set link to indivisual page

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -3,5 +3,7 @@ class TextsController < ApplicationController
     @texts = Text.includes(:users, :read_progresses).where(genre: Text::RAILS_GENRE_LIST)
   end
 
-  def show; end
+  def show
+    @text = Text.find(params[:id])
+  end
 end
